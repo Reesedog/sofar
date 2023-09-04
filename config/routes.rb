0f1c15f5bc15961ffa5481b06abbc00c9128e752
Reesedog <hghghghg110@126.com>
2023-09-04 09:15:31 +0000

    minor duplicate removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
 
   mount_devise_token_auth_for 'User', at: 'auth'
-
-  mount_devise_token_auth_for 'User', at: 'auth'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :entries, only: [:index, :create, :destroy]
   # Defines the root path route ("/")
